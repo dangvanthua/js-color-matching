8249620b8f4502ba1a6f27c06b1d31d3b324cd23
dangvanthua <thuandang021102@gmail.com>
2023-04-13 22:10:25 +0700

    handle event timer display text timer and show replay button

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
-import { GAME_STATUS } from './constants'
+import { GAME_STATUS, GAME_TIME, PAIRS_COUNT } from './constants.js'
+import { getColorBackground, getColorElementList, getColorListElement, getInActiveColorList, getPlayAgainButton } from './selectors.js';
+import { createTimer, getRandomColorPairs, hidePlayAgainButton, setTimmerText, showPlayAgainButton } from './utils.js'
 
 // Global variables
 let selections = []
 let gameState = GAME_STATUS.PLAYING
+let timmer = createTimer({
+    seconds: GAME_TIME,
+    onChange: handleTimmerChange,
+    onFinish: handleTimmerFinish
+});
+
+function handleTimmerChange(second) {
+    const fullSecond = `0${second}`.slice(-2);
+    setTimmerText(fullSecond)
+}
+
+function handleTimmerFinish() {
+    setTimmerText('Game Over 😭');
+    // set game status fisnish don't clicked by user
+    gameState = GAME_STATUS.FINISHED;
+    // show button replay game
+    showPlayAgainButton();
+}
 
 // TODOs
 // 1. Generating colors using https://github.com/davidmerfield/randomColor
@@ -10,3 +30,133 @@ let gameState = GAME_STATUS.PLAYING
 // 3. Check win logic
 // 4. Add timer
 // 5. Handle replay click
+
+console.log(getRandomColorPairs(PAIRS_COUNT));
+
+function initColor() {
+    // random 8 pairs of colors
+    const colorList = getRandomColorPairs(PAIRS_COUNT);
+
+    // bind to li > div.overlay
+    const liElementList = getColorElementList();
+
+    if (!liElementList) return;
+    // iterator throught li element tag and attach a color into li element
+    liElementList.forEach((liElement, index) => {
+        liElement.dataset.color = colorList[index];
+        const overlayElement = liElement.querySelector('.overlay');
+
+        if (overlayElement) overlayElement.style.backgroundColor = colorList[index];
+    })
+}
+
+function handleColorEvent(liElement) {
+    const shouldBlockClick = [GAME_STATUS.BLOCKING, GAME_STATUS.FINISHED].includes(gameState);
+    const isClicked = liElement.classList.contains('active');
+    if (!liElement || shouldBlockClick || isClicked) return;
+    liElement.classList.add('active');
+
+    selections.push(liElement);
+    if (selections.length < 2) return;
+
+    const firstColor = selections[0].dataset.color;
+    const secondColor = selections[1].dataset.color;
+    const isMatch = firstColor === secondColor;
+
+    if (isMatch) {
+        const backgroundElement = getColorBackground();
+
+        // set background color for section element
+        if (backgroundElement) {
+            backgroundElement.style.backgroundColor = selections[0].dataset.color;
+        }
+
+        const isWin = getInActiveColorList().length === 0;
+        if (isWin) {
+            // finish timer
+            timmer.clear();
+            // show replay
+            showPlayAgainButton();
+            // show you win
+            setTimmerText('Your Win 🥇');
+
+            // upadete status for player win
+            gameState = GAME_STATUS.FINISHED;
+        }
+
+        selections = [];
+        return;
+    }
+
+    gameState = GAME_STATUS.BLOCKING;
+
+    setTimeout(() => {
+        selections[0].classList.remove('active');
+        selections[1].classList.remove('active');
+
+        // set selections empty
+        selections = [];
+        if (gameState !== GAME_STATUS.FINISHED) {
+            gameState = GAME_STATUS.PLAYING;
+        }
+
+    }, 500)
+}
+
+function attachEventForColorList() {
+    // get ul tag list element for a event
+    const ulElement = getColorListElement();
+
+    if (!ulElement) return;
+
+    ulElement.addEventListener('click', (event) => {
+        if (event.target.tagName !== 'LI') return;
+        handleColorEvent(event.target)
+    });
+}
+
+function resetGame() {
+    // reset global variable
+
+    gameState = GAME_STATUS.PLAYING;
+    selections = [];
+
+    // reset DOMS element
+    const colorElementList = getColorElementList();
+    for (const colorElement of colorElementList) {
+        colorElement.classList.remove('active');
+    }
+
+
+    setTimeout(() => {
+        hidePlayAgainButton();
+        // set text title empty
+        setTimmerText('');
+        //re-generate new colors
+        initColor();
+        // replay time 
+        startTimmer();
+    }, 300)
+}
+
+function attachEventPlayAgainForButton() {
+    const playAgainButton = getPlayAgainButton();
+
+    if (playAgainButton) {
+        playAgainButton.addEventListener('click', resetGame)
+    }
+}
+
+function startTimmer() {
+    timmer.start();
+}
+
+(() => {
+    initColor();
+
+    attachEventForColorList();
+
+    attachEventPlayAgainForButton();
+
+    startTimmer();
+})()
--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,83 @@
+import { getPlayAgainButton, getTimerElement } from "./selectors.js";
+
+function shuffle(arr) {
+  if (!Array.isArray(arr) || arr.length <= 2) return;
+
+  for (let i = arr.length - 1; i > 1; i--) {
+    // calculate random value
+    let j = Math.floor(Math.random() * i);
+
+    // Replace value of arr by random
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+}
+
 export const getRandomColorPairs = (count) => {
   // receive count --> return count * 2 random colors
   // using lib: https://github.com/davidmerfield/randomColor
+
+  const colorList = [];
+  const heuList = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'monochrome',];
+
+  for (let i = 0; i < count; i++) {
+    const color = window.randomColor({
+      luminosity: 'dark',
+      hue: heuList[i % heuList.length],
+    });
+
+    colorList.push(color);
+  }
+  const fullColorList = [...colorList, ...colorList];
+
+  shuffle(fullColorList);
+
+  return fullColorList;
+}
+
+export function showPlayAgainButton() {
+  const playAgainButton = getPlayAgainButton();
+  if (playAgainButton) playAgainButton.classList.add('show');
+}
+
+export function hidePlayAgainButton() {
+  const playAgainButton = getPlayAgainButton();
+  if (playAgainButton) playAgainButton.classList.remove('show');
+}
+
+export function setTimmerText(text) {
+  const timmerText = getTimerElement();
+  if (timmerText) timmerText.textContent = text;
+}
+
+export function createTimer({ seconds, onChange, onFinish }) {
+  let intervalId = null;
+
+  function start() {
+    clear();
+
+    let currentSecond = seconds;
+    intervalId = setInterval(() => {
+      if (onChange) {
+        onChange(currentSecond);
+      }
+      currentSecond--;
+      if (currentSecond < 0) {
+        clear();
+        if (onFinish) {
+          onFinish();
+        }
+      }
+    }, 1000);
+  }
+
+  function clear() {
+    clearInterval(intervalId);
+  }
+
+  return {
+    start,
+    clear,
+  }
 }
